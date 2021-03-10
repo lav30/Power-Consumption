f79bb0b22c2420fc96a1c9e52f3a981c2f1183a2
Lavanya Santhosh <49737012+lav30@users.noreply.github.com>
2021-03-10 11:26:57 -0800

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ from flask import Flask,request, url_for, redirect, render_template, jsonify
 from pycaret.regression import *
 import pandas as pd
 import numpy as np
-
+import openpyxl
+from openpyxl import load_workbook
 
 app = Flask(__name__)
 
-df = pd.read_excel('Folds5x2_pp.xlsx')
+df = load_workbook(filename ='Folds5x2_pp.xlsx')
 model = load_model('Final XGBoost Model 3March2021')
 cols = ['AT', 'V', 'AP', 'RH']
 data = df.sample(frac=0.8, random_state=786)
